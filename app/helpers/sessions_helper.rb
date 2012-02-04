f84e54631da36fb1e2942bcfc41742789b86d426
Michael Kelly <michaelkellyclare@gmail.com>
2012-02-04 21:25:46 +0000

    Finished 10.2.2 Authentication

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,14 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
+
   private
 
     def user_from_remember_token
