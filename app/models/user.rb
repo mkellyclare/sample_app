b07f4f11a4b4d63af19f6d2931fddf57e1758184
Michael Kelly <michaelkellyclare@gmail.com>
2012-02-18 23:02:11 +0000

    chap 12 pt1

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,14 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed                           
+  
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
@@ -44,6 +52,20 @@ class User < ActiveRecord::Base
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
   end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
+
+
 
   def feed
     # This is preliminary. See Chapter 12 for the full implementation.
